c42d7a7e490ff4a60cb863254aca992a8d45c60e
Adam <adamartzi5136@gmail.com>
2022-11-29 17:23:07 +0200

    added func startGame, :quit and invalid input

--- game/gameCondition.py
+++ game/gameCondition.py
@@ -3,65 +3,65 @@ def check(ph1,ph2,ph3,ph4,ph5,ph6,ph7,ph8,ph9) -> bool:
     WonOrTie = False
 
 
-    if ph1 == "*****" and ph4 == "*****" and ph7 == "*****":
+    if ph1 == "XXXXX" and ph4 == "XXXXX" and ph7 == "XXXXX":
         WonOrTie = True
-        print("***** Won")
+        print("XXXXX Won")
     elif ph1 == "OOOOO" and ph4 == "OOOOO" and ph7 == "OOOOO":
         WonOrTie = True
         print("OOOOO Won")
-    if ph2 == "*****" and ph5 == "*****" and ph8 == "*****":
+    if ph2 == "XXXXX" and ph5 == "XXXXX" and ph8 == "XXXXX":
         WonOrTie = True
-        print("***** Won")
+        print("XXXXX Won")
     elif ph2 == "OOOOO" and ph5 == "OOOOO" and ph8 == "OOOOO":
         WonOrTie = True
         print("OOOOO Won")
 
     
-    if ph3 == "*****" and ph6 == "*****" and ph9 == "*****":
+    if ph3 == "XXXXX" and ph6 == "XXXXX" and ph9 == "XXXXX":
         WonOrTie = True
-        print("***** Won")
+        print("XXXXX Won")
     elif ph3 == "OOOOO" and ph6 == "OOOOO" and ph9 == "OOOOO":
         WonOrTie = True
         print("OOOOO Won")
 
 
 
-    if ph1 == "*****" and ph2 == "*****" and ph3 == "*****":
+    if ph1 == "XXXXX" and ph2 == "XXXXX" and ph3 == "XXXXX":
         WonOrTie = True
-        print("***** Won")
+        print("XXXXX Won")
     elif ph1 == "OOOOO" and ph2 == "OOOOO" and ph3 == "OOOOO":
         WonOrTie = True
         print("OOOOO Won")
 
-    if ph4 == "*****" and ph5 == "*****" and ph6 == "*****":
+    if ph4 == "XXXXX" and ph5 == "XXXXX" and ph6 == "XXXXX":
         WonOrTie = True
-        print("***** Won")
+        print("XXXXX Won")
     elif ph4 == "OOOOO" and ph5 == "OOOOO" and ph6 == "OOOOO":
         WonOrTie = True
         print("OOOOO Won")
 
-    if ph7 == "*****" and ph8 == "*****" and ph9 == "*****":
+    if ph7 == "XXXXX" and ph8 == "XXXXX" and ph9 == "XXXXX":
         WonOrTie = True
-        print("***** Won")
+        print("XXXXX Won")
     elif ph7 == "OOOOO" and ph8 == "OOOOO" and ph9 == "OOOOO":
         WonOrTie = True
         print("OOOOO Won")
 
 
 
-    if ph1 == "*****" and ph5 == "*****" and ph9 == "*****":
+    if ph1 == "XXXXX" and ph5 == "XXXXX" and ph9 == "XXXXX":
         WonOrTie = True
-        print("***** Won")
+        print("XXXXX Won")
     elif ph1 == "OOOOO" and ph5 == "OOOOO" and ph9 == "OOOOO":
         WonOrTie = True
         print("OOOOO Won")
 
-    if ph3 == "*****" and ph5 == "*****" and ph7 == "*****":
+    if ph3 == "XXXXX" and ph5 == "XXXXX" and ph7 == "XXXXX":
         WonOrTie = True
-        print("***** Won")
+        print("XXXXX Won")
     elif ph3 == "OOOOO" and ph5 == "OOOOO" and ph7 == "OOOOO":
         WonOrTie = True
         print("OOOOO Won")
 
-
+    
     return WonOrTie
--- game/main.py
+++ game/main.py
@@ -1,5 +1,7 @@
 import gameCondition
 
+print("The XXXXX player starts. Type :quit to quit and :replay to restart the game.")
+
 player = 1 #this means starting player is "*"
 input_number = 0
 ph1 = "-----"
@@ -11,7 +13,6 @@ ph6 = "-----"
 ph7 = "-----"
 ph8 = "-----"
 ph9 = "-----"
-WonOrTie = False
 
 top = ("      1        2        3")
 A = ("A : %s    %s    %s")
@@ -25,15 +26,18 @@ print(C%(ph7, ph8, ph9))
 
 def main(player):
     global ph1,ph2,ph3,ph4,ph5,ph6,ph7,ph8,ph9
+    global playerinput
+
     playerinput = str(input(""))
+    
     if player == 0:
         playerPH = "-----"
     elif player == 1:
-        playerPH = "*****"
-        print("***** It's you're turn")
+        playerPH = "XXXXX"
+        print("OOOOO It's you're turn")
     elif player == 2:
         playerPH = "OOOOO"
-        print("OOOOO It's you're turn")
+        print("XXXXX It's you're turn") #"its you're turn" is reverses, so XXXXX is placed in OOOOO and OOOOO placed in XXXXX XXX
         
     if playerinput[0] == "A":
         if playerinput [1] == "1":
@@ -56,6 +60,16 @@ def main(player):
             ph8 = "{0}".format(playerPH)
         if playerinput [1] == "3":
             ph9 = "{0}".format(playerPH)
+    
+    if playerinput == ":quit":
+        print("\n")
+        print("Ending Game...")
+        print("\n")
+        exit()
+    else: 
+        print("\n")
+        print("Invalid input, try again.")
+        print("\n")
 
     print(top)
     print(A%(ph1, ph2, ph3))
@@ -64,7 +78,13 @@ def main(player):
 
     playerPH = 2
 
-while WonOrTie == False:
-    main(1)
-    WonOrTie = gameCondition.check(ph1,ph2,ph3,ph4,ph5,ph6,ph7,ph8,ph9)
-    main(2)
+def startGame():
+    global WonOrTie
+    WonOrTie = False
+    while WonOrTie == False:
+        main(1)
+        WonOrTie = gameCondition.check(ph1,ph2,ph3,ph4,ph5,ph6,ph7,ph8,ph9)
+        main(2)
+
+startGame()
+
